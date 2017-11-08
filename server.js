b0ac79f2ba25fa62bd11d72786b847b55d10f551
Jawaid Gadiwala <jawaidgadiwala@gmail.com>
2017-11-08 13:05:22 +0500

    Hello World

---
+++ server.js
@@ -0,0 +1,32 @@
+'use strict';
+
+require('dotenv').config();
+var express = require('express');
+var app = express();
+var http = require('http');
+
+global.appEnv = function (name, def) {
+    var val = undefined;
+    if (process.env[name] === undefined) {
+        val == def;
+    }
+    else {
+        val = process.env[name];
+    }
+
+    if (typeof val == "string") {
+        if (val.toLowerCase() == "true") {
+            val = true;
+        }
+        else if (val.toLowerCase() == "false") {
+            val = false;
+        }
+    }
+    return val;
+};
+
+http.createServer(app).listen(appEnv('HTTP_PORT', 80), function () {
+    console.log("App server listening on port " + appEnv('HTTP_PORT', 80));
+});
+
+app.get('/', (req, res) => res.send('Hello World!'))
